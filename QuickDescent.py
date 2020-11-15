aebf43e9d98ce06ad5b35948e7f23f18a4d43749
nickiteks <55086354+nickiteks@users.noreply.github.com>
2020-11-15 22:41:44 +0400

    lab 4 for dminr

--- QuickDescent.py
+++ QuickDescent.py
@@ -12,9 +12,8 @@ class qiuck:
         iteration = 1
         trigger = True
         while(trigger):
-            Xes[0].append(Xes[0][iteration-1]-alfa*(2*(math.sin(Xes[0][iteration-1])+2*Xes[1][iteration-1]-2)*(math.cos(Xes[0][iteration-1]))+2*(math.cos(Xes[0][iteration-1])+Xes[1][iteration-1]-1.5)*(-math.sin(Xes[0][iteration-1]))))
-            Xes[1].append(Xes[1][iteration-1]-alfa*(2*(math.sin(Xes[0][iteration-1])+2*Xes[1][iteration-1]-2)*2+2*(math.cos(Xes[0][iteration-1])+Xes[1][iteration-1]-1.5)))
-
+            Xes[0].append(Xes[0][iteration-1]-alfa*(2*(math.sin(Xes[0][iteration-1]-1)+Xes[1][iteration-1]-0.1)*(math.cos(Xes[0][iteration-1]-1))+2*(Xes[0][iteration-1]-math.sin(Xes[1][iteration-1]+1)-0.8)))
+            Xes[1].append(Xes[1][iteration-1]-alfa*(2*(math.sin(Xes[0][iteration-1]-1)+Xes[1][iteration-1]-0.1)*1+2*(Xes[0][iteration-1]-math.sin(Xes[1][iteration-1]+1)-0.8)*(math.cos(Xes[1][iteration-1]+1))))
             #|xk - xk-1|
             raznX1 = math.fabs(Xes[0][iteration]-Xes[0][iteration-1])
             raznX2 = math.fabs(Xes[1][iteration]-Xes[1][iteration-1])
@@ -35,3 +34,6 @@ class qiuck:
         Xes[0][len(Xes[0]) - 1] = round(Xes[0][len(Xes[0]) - 1], num)
         Xes[1][len(Xes[1]) - 1] = round(Xes[1][len(Xes[1]) - 1], num)
         print("x1 = " + str(Xes[0][len(Xes[0]) - 1]) + "   x2 = " + str(Xes[1][len(Xes[1]) - 1]))
+
+        print(Normas[len(Normas)-1])
+        print (iteration)
